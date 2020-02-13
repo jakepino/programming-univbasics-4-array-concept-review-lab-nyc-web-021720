3d348c15279c2ff1cd2a482aa85b012db04bd96c
Jake Pino <jakepino@yahoo.com>
2020-02-13 20:55:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,8 @@ def find_element_index(array, value_to_find)
   array.length.times { |index| 
     if array[index] == value_to_find
       return array.index(value_to_find)
+    else
+      return nil
     end
   }
 end
