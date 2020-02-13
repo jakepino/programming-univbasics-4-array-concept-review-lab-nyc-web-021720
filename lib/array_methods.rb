826230da3622738b6a0874b5f2ac9cbc60ab9f6f
Jake Pino <jakepino@yahoo.com>
2020-02-13 21:07:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  max = array[0]
+  array.length { |val|
+    if val > max
+      max = val
+    end
+  }
+  return max
 end
 
 def find_min_value(array)
