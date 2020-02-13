a6cc21c61fa65e7fbb43794d1e0dc967abb0689d
Jake Pino <jakepino@yahoo.com>
2020-02-13 20:58:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,10 +2,9 @@ def find_element_index(array, value_to_find)
   array.length.times { |index| 
     if array[index] == value_to_find
       return array.index(value_to_find)
-    else
-      return nil
     end
   }
+  return nil
 end
 
 def find_max_value(array)
